723f6760fc4e29e04879355cbbe178eeeb38a140
Junhyeong Hong <hjh5696@gmail.com>
2022-09-29 15:09:20 +0100

    t

--- type.ts
+++ type.ts
@@ -2,3 +2,9 @@ export interface TestObject {
     x: number;
     y: string;
 }
+
+export const testFunction = (a: TestObject) => {
+    return a.x + 1;
+}
+
+console.log(testFunction({x: 1, y: 1}))
